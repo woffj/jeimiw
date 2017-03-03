0b88574a5582bb3e70bf0a0d74f2d8d92c8fe98e
woffj <jeimi@yahoo.cin>
2017-03-03 13:39:50 -0700

    Analytics Tracking

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,17 @@
     <!-- Color CSS -->
     <link href="assets/css/colors/green.css" rel="stylesheet" type="text/css">
 
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-33516773-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+
     <!-- Modernizr JS for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 8]>
     <script src="assets/plugins/modernizr.min.js"></script>
